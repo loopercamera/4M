7ded3517f3a1495dfbd6abee4b368389b67e23bd
loopercamera <fabian.gross@students.fhnw.ch>
2025-04-02 12:04:27 +0200

    resolve ETL Erorr

--- 01_ETL/ETL_script.py
+++ 01_ETL/ETL_script.py
@@ -48,59 +48,56 @@ transform_opendata_swiss(r"01_ETL\01_opendata.swiss")
 # # 2. Process geocat.ch
 # # -------------------------------
 
-<<<<<<< Updated upstream
 # 2.1 Download metadata
-from download_geocat import download_geocat_metadata
-download_geocat_metadata(
-    save_dir=r"01_ETL\02_geocat.ch\saved_metadata_xml",
-    batch_size=100,
-    wait_time=1,
-    max_records=None  # Set to None to download everything
-)
-=======
-# # 2.1 Download metadata
-# # from download_geocat import download_geocat_metadata
-# # download_geocat_metadata(
-# #     save_dir=r"01_ETL\02_geocat.ch\saved_metadata_xml",
-# #     batch_size=100,
-# #     wait_time=1,
-# #     max_records=None  # Set to None to download everything
-# # )
->>>>>>> Stashed changes
+# from download_geocat import download_geocat_metadata
+# download_geocat_metadata(
+#     save_dir=r"01_ETL\02_geocat.ch\saved_metadata_xml",
+#     batch_size=100,
+#     wait_time=1,
+#     max_records=None  # Set to None to download everything
+# )
+# 2.1 Download metadata
+# from download_geocat import download_geocat_metadata
+# download_geocat_metadata(
+#     save_dir=r"01_ETL\02_geocat.ch\saved_metadata_xml",
+#     batch_size=100,
+#     wait_time=1,
+#     max_records=None  # Set to None to download everything
+# )
 
-# # 2.2 Clean metadata
-# # xml_cleaner.PORTAL_NAME = "geocat.ch"
-# # xml_cleaner.process_folders([
-# #     r"01_ETL\02_geocat.ch\saved_metadata_xml"
-# # ])
+# 2.2 Clean metadata
+# xml_cleaner.PORTAL_NAME = "geocat.ch"
+# xml_cleaner.process_folders([
+#     r"01_ETL\02_geocat.ch\saved_metadata_xml"
+# ])
 
-# # 2.3 Compare metadata
-# # compare_dataset_hashes(
-# #     base_dir=r"01_ETL\02_geocat.ch",
-# #     remove_order_file=r"01_ETL\removeorder_metadata_geocat.ch.csv",
-# #     extension="xml",
-# #     portal_name="geocat.ch"
-# # )
+# 2.3 Compare metadata
+# compare_dataset_hashes(
+#     base_dir=r"01_ETL\02_geocat.ch",
+#     remove_order_file=r"01_ETL\removeorder_metadata_geocat.ch.csv",
+#     extension="xml",
+#     portal_name="geocat.ch"
+# )
 
-# # 2.4 Extract and export metadata
-# # from extract_metadata_geocat import extract_and_save_all_geocat
-# # extract_and_save_all_geocat(
-# #     input_folder=r"01_ETL\02_geocat.ch\saved_metadata_xml",
-# #     output_folder=r"01_ETL\02_geocat.ch"
-# # )
+# 2.4 Extract and export metadata
+# from extract_metadata_geocat import extract_and_save_all_geocat
+# extract_and_save_all_geocat(
+#     input_folder=r"01_ETL\02_geocat.ch\saved_metadata_xml",
+#     output_folder=r"01_ETL\02_geocat.ch"
+# )
 
-# # 2.5 Transform extracted metadata
-# # from transform_metadata_geocat import process_dataset_metadata, clean_csv_file
+# 2.5 Transform extracted metadata
+# from transform_metadata_geocat import process_dataset_metadata, clean_csv_file
 
-# # files = [
-# #     r"01_ETL\02_geocat.ch\geocat_dataset_metadata.csv",
-# #     r"01_ETL\02_geocat.ch\geocat_distribution_metadata.csv",
-# #     r"01_ETL\02_geocat.ch\geocat_contact_point_metadata.csv"
-# # ]
+# files = [
+#     r"01_ETL\02_geocat.ch\geocat_dataset_metadata.csv",
+#     r"01_ETL\02_geocat.ch\geocat_distribution_metadata.csv",
+#     r"01_ETL\02_geocat.ch\geocat_contact_point_metadata.csv"
+# ]
 
-# # for f in files:
-# #     clean_csv_file(f)
-# #     process_dataset_metadata(f)
+# for f in files:
+#     clean_csv_file(f)
+#     process_dataset_metadata(f)
 
 # # -------------------------------
 # # 3. Merge All Metadata
